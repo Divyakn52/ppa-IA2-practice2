4f9aff119d0b39015b3cd9c70f20540078ac5a29
Divzz <97013125+Divyakn52@users.noreply.github.com>
2022-02-09 05:32:52 +0000

    coding

--- p1original.c
+++ p1original.c
@@ -1,2 +1,22 @@
-
+#include<stdio.h>
+void input(float *base,float *height)
+{
+  printf("enter b,h values\n");
+  scanf("%f %f",base,height);
+}
+void find_area(float base,float height,float *area)
+{
+ *area=(base*height)/2;
+}
+void output(float base,float height,float area)
+{
+  printf("the area of the triangle is %f",area);
+}
+int main()
+{
+ int b,h,area;
+ input(&b,&h);
+ find_area(b,h,&area);
+ output(b,h,area);
+ return 0;
 }
