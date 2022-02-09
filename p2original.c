2c372ab94fe281dc8e979e8844c9710df8b00ce3
Divzz <97013125+Divyakn52@users.noreply.github.com>
2022-02-09 06:53:45 +0000

    coding

---
+++ p2original.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int input_side()
+{
+  int x;
+  printf("enter value of three sides of triangle\n");
+  scanf("%d",&x);
+  return x;
+}
+int check_scalene(int a,int b,int c)
+{
+ if(a==b&&b==c)
+ {
+   printf("all sides are equal\n");
+ }
+ else if(a==b||a==c||b==c)
+ {
+   printf("two sides are equal\n");
+ }
+ else
+ {
+   printf("it's a scalene triangle\n");
+ }
+ return 0;
+}
+void output(int a,int b,int c,int isscalene)
+{
+  printf("%d\n",isscalene);
+}
+int main()
+{
+  int a,b,c,isscalene;
+  a=input_side();
+  b=input_side();
+  c+input_side();
+  is scalene=check_scalene
+  output(a,b,c,is scalene);
+  return 0;
+}
